94dd531246e9d66ba0d8ef779c00dbfe7175b1b6
liensanchez <liensilviosanchez@gmail.com>
2023-01-20 14:33:22 -0300

    saque comentarios

--- api/src/controllers/dogs.Controllers.js
+++ api/src/controllers/dogs.Controllers.js
@@ -5,7 +5,6 @@ const allDogsInfo = async () => {
 
   const dogsAPI = await axios.get('https://api.thedogapi.com/v1/breeds');
 
-  //lo transformamos para mostrar correctamente la info
   let allDogsInfo = await (dogsAPI.data)
   
   return allDogsInfo
@@ -17,18 +16,19 @@ const searchDog = async (dogName) => {
   let dog = dogName 
 
   const dogAPI = await axios.get(`https://api.thedogapi.com/v1/breeds/search?q=${dog}`);
-  //lo transformamos para mostrar correctamente la info
+
   let theDogInfo = await (dogAPI.data)
 
   return theDogInfo
 }
+
 
 const searchID = async (dogId) => {
 
   let dog = dogId 
 
   const dogAPI = await axios.get(`https://api.thedogapi.com/v1/breeds/${dog}`);
-  //lo transformamos para mostrar correctamente la info
+
   let theDogInfo = await (dogAPI.data)
 
   return theDogInfo
